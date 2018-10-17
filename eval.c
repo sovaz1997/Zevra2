e92d1f95b2df0d4de09d91201ccf78b7715bce21
sovaz1997 <sovaz1997@yandex.ru>
2018-10-17 09:08:33 +0300

    Добавлена оценка мобильности, исправлены веса фигур

--- eval.c
+++ eval.c
@@ -1,11 +1,23 @@
 #include "eval.h"
 
+int QueenMobility[28] = {
+    -30, -20, -10, 0, 5, 10, 12, 15, 18, 20, 25, 30, 32, 35,
+    40, 45, 50, 55, 57, 60, 63, 65, 70, 75, 80, 85, 90, 95
+};
+int RookMobility[15] = {-30, -20, -10, 0, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 80};
+int BishopMobility[14] = {-30, -10, 5, 15, 20, 25, 35, 40, 45, 50, 55, 60, 65, 70};
+int KnightMobility[14] = {-50, -25, -10, -2, 5, 10, 15, 25};
+
 int fullEval(Board* board) {
     int eval = 0;
 
     //Базовая оценка
     eval += materialEval(board);
     eval += psqtEval(board);
+
+    //Оценка мобильности
+    eval += mobilityEval(board, WHITE);
+    eval -= mobilityEval(board, BLACK);
 
     return (board->color == WHITE ? eval : -eval);
 }
@@ -69,6 +81,65 @@ int psqtPieceEval(Board* board, U64 mask, const int* pstTable) {
             eval -= *(pstTable + sq);
         }
         clearBit(&mask, sq);
+    }
+
+    return eval;
+}
+
+int mobilityEval(Board* board, int color) {
+    int eval = 0;
+
+    U64 our = board->colours[color]; //наши фигуры
+    U64 enemy = board->colours[!color]; //чужие фигуры
+    U64 occu = (our | enemy); //все фигуры
+
+    U64 mask = board->pieces[PAWN] & enemy;
+    U64 pawnAttacks;
+    if(!color == WHITE) {
+        pawnAttacks = ((mask << 9) & ~files[0]) | ((mask << 7) & ~files[7]);
+    } else {
+        pawnAttacks = ((mask >> 9) & ~files[7]) | ((mask >> 7) & ~files[0]);
+    }
+    U64 possibleSq = ~pawnAttacks;
+
+    //Ладья
+    mask = board->pieces[ROOK] & our;
+
+    while(mask) {
+        int from = firstOne(mask);
+        U64 possibleMoves = rookPossibleMoves[from][getMagicIndex(occu & rookMagicMask[from] & unSquareBitboard[from], rookMagic[from], rookPossibleMovesSize[from])];
+        eval += RookMobility[popcount(possibleMoves & possibleSq)];
+        clearBit(&mask, from);
+    }
+
+    //Слон
+    mask = board->pieces[BISHOP] & our;
+
+    while(mask) {
+        int from = firstOne(mask);
+        U64 possibleMoves = bishopPossibleMoves[from][getMagicIndex(occu & bishopMagicMask[from] & unSquareBitboard[from], bishopMagic[from], bishopPossibleMovesSize[from])];
+        eval += BishopMobility[popcount(possibleMoves & possibleSq)];
+        clearBit(&mask, from);
+    }
+
+    //Ферзь
+    mask = board->pieces[QUEEN] & our;
+
+    while(mask) {
+        int from = firstOne(mask);
+        U64 possibleMoves = rookPossibleMoves[from][getMagicIndex(occu & rookMagicMask[from] & unSquareBitboard[from], rookMagic[from], rookPossibleMovesSize[from])];
+        eval += QueenMobility[popcount(possibleMoves & possibleSq)];
+        clearBit(&mask, from);
+    }
+
+    //Конь
+    mask = board->pieces[KNIGHT] & our;
+
+    while(mask) {
+        int from = firstOne(mask);
+        U64 possibleMoves = knightAttacks[from];
+        eval += KnightMobility[popcount(possibleMoves & possibleSq)];
+        clearBit(&mask, from);
     }
 
     return eval;
--- eval.h
+++ eval.h
@@ -7,15 +7,22 @@
 //Веса фигур
 enum figureWeights {
     PAWN_EV = 100,
-    KNIGHT_EV = 400,
-    BISHOP_EV = 430,
-    ROOK_EV = 650,
-    QUEEN_EV = 1100
+    KNIGHT_EV = 300,
+    BISHOP_EV = 330,
+    ROOK_EV = 550,
+    QUEEN_EV = 1000
 };
+
+extern int QueenMobility[28];
+extern int RookMobility[15];
+extern int BishopMobility[14];
+extern int KnightMobility[14];
+
 
 int fullEval(Board* board);
 int materialEval(Board* board);
 int psqtEval(Board* board);
 int psqtPieceEval(Board* board, U64 mask, const int* pstTable);
+int mobilityEval(Board* board, int color);
 
 #endif
