d2b83a04ea70b61937f215b0076eb92783749d7c
Oleg Smirnov <sovaz1997@yandex.ru>
2018-11-21 20:30:49 +0300

    fix

--- bitboards.c
+++ bitboards.c
@@ -168,10 +168,10 @@ void attacksGen() {
             pawnAttacks[WHITE][sq] |= ((1ull << (sq + 7)) & ~files[7]);
         }
 
-        if(sq - 9 < 64) {
+        if(sq - 9 >= 0) {
             pawnAttacks[BLACK][sq] |= ((1ull << (sq - 9)) & ~files[7]);
         }
-        if(sq - 7 < 64) {
+        if(sq - 7 >= 0) {
             pawnAttacks[BLACK][sq] |= ((1ull << (sq - 7)) & ~files[0]);
         }
     }
