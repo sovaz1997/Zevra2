3a25aeed2854a276fb2c6d0dce00d54538122b08
sovaz1997 <sovaz1997@yandex.ru>
2021-08-24 19:37:27 +0300

    fix: исправление оценки сильных пешек

--- src/eval.c
+++ src/eval.c
@@ -154,16 +154,37 @@ int pawnsEval(Board* board, int color) {
     //isolated pawn bonus
     eval += (IsolatedPawnsHash[horizontalScan(ourPawns)]);
 
+
     //passed pawn bonus
     while(ourPawns) {
         int sq = firstOne(ourPawns);
-        if(color == WHITE) {
-            if(!(plus8[sq] & enemyPawns))
-                eval += getPassedPawnBonus(sq, color);
-        } else {
-            if(!(minus8[sq] & enemyPawns))
-                eval += getPassedPawnBonus(sq, color);
+
+        if(!((color == WHITE ? plus8 : minus8)[sq] & enemyPawns)) {
+            eval += getPassedPawnBonus(sq, color);
         }
+
+        int advantage = 0;
+
+        if (fileOf(sq) > 0) {
+            if (!((color == WHITE ? plus8 : minus8)[sq - 1] & enemyPawns)) {
+                advantage++;
+            }
+        } else {
+            advantage++;
+        }
+
+        if (fileOf(sq) < 7) {
+            if (!((color == WHITE ? plus8 : minus8)[sq + 1] & enemyPawns)) {
+                advantage++;
+            }
+        } else {
+            advantage++;
+        }
+
+        if (advantage == 2) {
+            eval += 8;
+        }
+
         clearBit(&ourPawns, sq);
     }
 
@@ -171,72 +192,7 @@ int pawnsEval(Board* board, int color) {
     for(int f = 0; f < 8; ++f)
         eval -= DoublePawnsPenalty * (popcount(ourPawns & files[f]) > 1);
 
-    U64 whitePawns = board->colours[WHITE] & board->pieces[PAWN];
-    U64 blackPawns = board->colours[BLACK] & board->pieces[PAWN];
-
-    U64 whitePawnsCopy = whitePawns;
-
-    int strongPawnsEval = 0;
-    while(whitePawnsCopy) {
-        int advantage = 0;
-        int sq = firstOne(whitePawnsCopy);
-
-        if (fileOf(sq) > 0) {
-            if (!(plus8[sq - 1] & blackPawns)) {
-                advantage++;
-            }
-        } else {
-            advantage++;
-        }
-
-        if (fileOf(sq) < 7) {
-            if (!(plus8[sq + 1] & blackPawns)) {
-                advantage++;
-            }
-        } else {
-            advantage++;
-        }
-        clearBit(&whitePawnsCopy, sq);
-
-        if (advantage == 2) {
-            strongPawnsEval += 8;
-        }
-    }
-
-
-    U64 blackPawnsCopy = blackPawns;
-
-    while(blackPawnsCopy) {
-        int advantage = 0;
-        int sq = firstOne(blackPawnsCopy);
-
-        if (fileOf(sq) > 0) {
-            if (!(minus8[sq - 1] & whitePawns)) {
-                advantage++;
-            }
-        } else {
-            advantage++;
-        }
-
-        if (fileOf(sq) < 7) {
-            if (!(minus8[sq + 1] & whitePawns)) {
-                advantage++;
-            }
-        } else {
-            advantage++;
-        }
-        clearBit(&blackPawnsCopy, sq);
-
-        if (advantage == 2) {
-            strongPawnsEval -= 8;
-        }
-    }
-
-    if (color == BLACK) {
-        strongPawnsEval = -strongPawnsEval;
-    }
-
-    return eval + strongPawnsEval;
+    return eval;
 }
 
 int bishopsEval(Board* board) {
