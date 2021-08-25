f691ae4140914b3c24ef8b7c82e238fe82629db4
sovaz1997 <sovaz1997@yandex.ru>
2021-08-25 19:20:55 +0300

    fix: Исправление

--- src/search.c
+++ src/search.c
@@ -235,18 +235,15 @@ int search(Board* board, SearchInfo* searchInfo, int alpha, int beta, int depth,
         if(movesCount == 1) {
             eval = -search(board, searchInfo, -beta, -alpha, nextDepth + extensions, height + 1);
         } else {
-            // TODO: enable zero-search window
             if(LmrPruningAllow && playedMovesCount >= 3 && quiteMove) {
                 eval = -search(board, searchInfo, -alpha - 1, -alpha, nextDepth + extensions - reductions, height + 1);
                 if(eval > alpha)
-                eval = -search(board, searchInfo, -beta, -alpha, nextDepth + extensions, height + 1);
+                    eval = -search(board, searchInfo, -beta, -alpha, nextDepth + extensions, height + 1);
             } else {
                 eval = -search(board, searchInfo, -alpha - 1, -alpha, nextDepth + extensions, height + 1);
     
                 if(eval > alpha && eval < beta)
                     eval = -search(board, searchInfo, -beta, -alpha, nextDepth + extensions, height + 1);
-                eval = -search(board, searchInfo, -beta, -alpha, nextDepth + extensions, height + 1);
-
             }
         }
         unmakeMove(board, *curMove, &undo);
