ffb585950c1b0f4cc2cbd9a5166029f78a8c4d4e
sovaz1997 <sovaz1997@yandex.ru>
2021-09-15 00:15:08 +0300

    feat: mcts simulations for root node

--- src/mcts.c
+++ src/mcts.c
@@ -1,11 +1,50 @@
 #include "mcts.h"
 #include "search.h"
 
+struct MCTSNode {
+    U16 move;
+    MCTSNode** children;
+    int childrenCount;
+    double w;
+    double n;
+};
+
+MCTSNode* createMCTSNode(U16 move) {
+    MCTSNode* node = (MCTSNode*)malloc(sizeof(MCTSNode));
+
+    node->move = move;
+    node->n = 0;
+    node->w = 0;
+    node->children = NULL;
+    node->childrenCount = 0;
+
+    return node;
+}
+
 U16 movesCash1[512];
 U16 movesCash2[512];
 
 int MCTSSearch(Board* board) {
-    simulate(board, movesCash1);
+    MCTSNode* root = createMCTSNode(0);
+    runSimulationsForNode(board, root);
+
+    for (int i = 0; i < root->childrenCount; ++i) {
+        printf("%f\n", root->children[i]->w);
+    }
+}
+
+void runSimulationsForNode(Board* board, MCTSNode* node) {
+    int movesCount = generatePossibleMoves(board, movesCash1);
+
+    node->children = malloc(sizeof(MCTSNode*) * movesCount);
+    node->childrenCount =movesCount;
+
+    for (int i = 0; i < movesCount; ++i) {
+        node->children[i] = createMCTSNode(movesCash1[i]);
+        node->children[i]->n++;
+        node->children[i]->w += simulate(board, movesCash2);
+        node->children[i]->move = movesCash1[i];
+    }
 }
 
 double simulate(Board* board, U16* movesCash) {
@@ -46,8 +85,8 @@ int generatePossibleMoves(Board* board, U16* moves) {
             ++movesCount;
         }
 
-        (*curMove)++;
         unmakeMove(board, *curMove, &undo);
+        curMove++;
     }
 
     possibleMove[movesCount] = 0;
--- src/mcts.h
+++ src/mcts.h
@@ -1,5 +1,10 @@
 #include "board.h"
 
+typedef struct MCTSNode MCTSNode;
+
+MCTSNode* createMCTSNode(U16 move);
+void runSimulationsForNode(Board* board, MCTSNode* node);
+void addChild(MCTSNode* parent, MCTSNode* child);
 int MCTSSearch();
 int generatePossibleMoves(Board* board, U16* moves);
 double simulate(Board* board, U16* movesCash);
