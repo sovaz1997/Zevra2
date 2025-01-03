8d2d211b94c99ec744bfe0dede3b4eaba8bd4e44
Oleg Smirnov <sovaz1997@yandex.ru>
2025-01-03 16:34:07 +0600

    feat: Start working on NNUE implementation

---
+++ src/nnue.h
@@ -0,0 +1,23 @@
+#include "board.h"
+
+const int INPUTS_COUNT = 768;
+const int INNER_LAYER_COUNT = 8;
+//const int ACCUMULATOR_COUNT = 1024;
+
+//const double WEIGHTS_1[INPUTS_COUNT][INNER_LAYER_COUNT];
+//const double WEIGHTS_2[INNER_LAYER_COUNT][INNER_LAYER_COUNT];
+//const double WEIGHTS_3[INNER_LAYER_COUNT];
+
+struct NNUE {
+    double inputs[INPUTS_COUNT];
+    double weights_1[INPUTS_COUNT][INNER_LAYER_COUNT];
+    double weights_2[INNER_LAYER_COUNT][INNER_LAYER_COUNT];
+    double weights_3[INNER_LAYER_COUNT];
+    double accumulator1[INNER_LAYER_COUNT];
+    double accumulator2[INNER_LAYER_COUNT];
+    double eval;
+};
+
+int isExists(Board* board, int color, int piece, int sq);
+int getInputIndexOf(int color, int piece, int sq);
+NNUE* createNNUE(Board* board);
--- src/tuning.c
+++ src/tuning.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <stdlib.h>
 
+
 #include "tuning.h"
 #include "search.h"
 
