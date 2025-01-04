ebdd6032d4fad28622987518141e646b04aa7863
Oleg Smirnov <sovaz1997@yandex.ru>
2025-01-04 06:56:05 +0600

    try to learn net

--- nnue-learn/main.py
+++ nnue-learn/main.py
@@ -1,12 +1,19 @@
 import math
 from typing import Generator
 
+from torch.utils.data import Dataset, DataLoader
+
 import chess
 import chess.pgn
 import chess.engine
 import csv
+import torch
+import torch.nn as nn
+import pandas as pd
+import torch.optim as optim
 
-DATASET_POSITIONS_COUNT = 300
+
+DATASET_POSITIONS_COUNT = 250000
 
 
 def process_large_pgn(file_path, output_file):
@@ -118,6 +125,98 @@ def evaluate_positions(file_path: str, output_csv_path: str):
                 engine = chess.engine.SimpleEngine.popen_uci("./zevra")
 
 
+# Создание датасета из CSV
+class ChessDataset(Dataset):
+    def __init__(self, file_path):
+        self.data = pd.read_csv(file_path)
+        self.board = chess.Board()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        fen = self.data.iloc[idx, 0]
+        score = self.data.iloc[idx, 1] / 1000
+        self.board.set_fen(fen)
+        inputs = calculate_nnue_input_layer(self.board)
+        return torch.tensor(inputs, dtype=torch.float32), torch.tensor(score, dtype=torch.float32)
+
+
+
+class NNUE(nn.Module):
+    def __init__(self, input_size=768, hidden_size=8, output_size=1):
+        super(NNUE, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
 if __name__ == '__main__':
-    print('Starting')
-    evaluate_positions("ccrl_positions.txt", "dataset.csv")
+    model = NNUE()
+    print(model)
+
+    dataset = ChessDataset("dataset.csv")
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=4)
+
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=2, factor=0.5)
+    # num_epochs = 10
+    epoch = 0
+    while True:
+        epoch += 1
+        model.train()
+        running_loss = 0.0
+        index = 0
+        for (batch_inputs, batch_scores) in dataloader:
+            index += 1
+            optimizer.zero_grad()
+            outputs = model(batch_inputs)
+            loss = criterion(outputs.squeeze(), batch_scores)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            # print(f"Batch loss: {loss.item()}", flush=True)
+            if index % 100 == 0:
+                print(f"Learning: {index}")
+        loss = running_loss / len(dataloader)
+        print(f"Epoch [{epoch}], Loss: {running_loss / len(dataloader):.4f}", flush=True)
+        if loss < 0.2:
+            break
+        # print LR
+        print(optimizer.param_groups[0]['lr'])
+
+
+
+
+
+    # for i, (batch_inputs, batch_scores) in enumerate(dataloader):
+    #     print(f"Batch {i + 1}:")
+    #     print("Inputs:", batch_inputs)
+    #     print("Scores:", batch_scores)
+    #     print("Batch inputs shape:", batch_inputs.shape)
+    #     print("Batch scores shape:", batch_scores.shape)
+    #     print("-" * 50)
+    #
+    #     # Ограничиваем количество выводимых батчей
+    #     if i >= 0:  # Печатаем только 2 батча
+    #         break
+
+
+    # for batch_inputs, batch_scores in dataloader:
+    #     print("Batch inputs shape:", batch_inputs.shape)
+    #     print("Batch scores shape:", batch_scores.shape)
+    #     break
+
+    # example_input = torch.rand((32, 768))
+    # output = model(example_input)
+    # print("Output shape:", output.shape)
+    # evaluate_positions("ccrl_positions.txt", "dataset.csv")
