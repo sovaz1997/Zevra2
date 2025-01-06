b9e8d11a434d11c23b4310f357852479072ed5ba
Oleg Smirnov <sovaz1997@yandex.ru>
2025-01-06 20:15:20 +0600

    quantization

--- src/nnue.h
+++ src/nnue.h
@@ -3,15 +3,19 @@
 
 #include "board.h"
 
-static const int INPUTS_COUNT = 768;
-static const int INNER_LAYER_COUNT = 64;
+#define INPUTS_COUNT 768
+#define INNER_LAYER_COUNT 32
+//static const int INPUTS_COUNT = 768;
+//static const int INNER_LAYER_COUNT = 32;
 
 
 struct NNUE {
-    double inputs[INPUTS_COUNT];
+    S16 inputs[INPUTS_COUNT];
     double weights_1[INNER_LAYER_COUNT][INPUTS_COUNT];
+    S16 weights_1_quantized[INNER_LAYER_COUNT][INPUTS_COUNT];
     double weights_2[INNER_LAYER_COUNT];
-    double accumulators[INNER_LAYER_COUNT];
+    S16 weights_2_quantized[INNER_LAYER_COUNT];
+    S32 accumulators[INNER_LAYER_COUNT];
     double eval;
 };
 
