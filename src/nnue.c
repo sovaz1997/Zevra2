381319a667eb71d6a27c8ebf8d6b60fe7fa8c509
Oleg Smirnov <sovaz1997@yandex.ru>
2025-02-23 23:32:10 +0300

    adapt to x86

--- src/nnue.c
+++ src/nnue.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <limits.h>
-#include <arm_neon.h>
+#ifdef __ARM_NEON
+    #include <arm_neon.h>
+#elif defined(__AVX2__)
+    #include <immintrin.h>
+#endif
 #include "nnue.h"
 #include "timemanager.h"
 #include "search.h"
@@ -22,6 +26,7 @@ double ReLU(double x) {
     return x > 0 ? x : 0;
 }
 
+#ifdef __ARM_NEON
 void recalculateEval(NNUE* nnue, int color) {
     int32x4_t sum_vec_low = vdupq_n_s32(0);
     int32x4_t sum_vec_high = vdupq_n_s32(0);
@@ -119,6 +124,75 @@ void resetNNUEInput(S16* inputs, S32* accumulators, S32 (*weights)[INNER_LAYER_C
         vst1q_s32(&accumulators[i + 4], acc_vec_high);
     }
 }
+
+
+#elif defined(__AVX2__)
+static inline int horizontal_sum(__m256i vec) {
+    __m128i low  = _mm256_castsi256_si128(vec);
+    __m128i high = _mm256_extracti128_si256(vec, 1);
+    __m128i sum128 = _mm_add_epi32(low, high);
+    sum128 = _mm_hadd_epi32(sum128, sum128);
+    sum128 = _mm_hadd_epi32(sum128, sum128);
+    return _mm_cvtsi128_si32(sum128);
+}
+
+void recalculateEval(NNUE* nnue, int color) {
+    __m256i sum_vec = _mm256_setzero_si256();
+    int shift = (color == WHITE) ? 0 : INNER_LAYER_COUNT;
+
+    for (int i = 0; i < INNER_LAYER_COUNT; i += 8) {
+        __m256i acc = _mm256_loadu_si256((__m256i*)&nnue->accumulators[i]);
+        __m256i w2  = _mm256_loadu_si256((__m256i*)&nnue->weights_2_quantized[shift + i]);
+        acc = _mm256_max_epi32(acc, _mm256_setzero_si256());
+        __m256i prod = _mm256_mullo_epi32(acc, w2);
+        sum_vec = _mm256_add_epi32(sum_vec, prod);
+    }
+    int part1 = horizontal_sum(sum_vec);
+
+    sum_vec = _mm256_setzero_si256();
+    shift = (color == WHITE) ? INNER_LAYER_COUNT : 0;
+    for (int i = 0; i < INNER_LAYER_COUNT; i += 8) {
+        __m256i acc = _mm256_loadu_si256((__m256i*)&nnue->accumulators_perspective[i]);
+        __m256i w2  = _mm256_loadu_si256((__m256i*)&nnue->weights_2_quantized[i + shift]);
+        acc = _mm256_max_epi32(acc, _mm256_setzero_si256());
+        __m256i prod = _mm256_mullo_epi32(acc, w2);
+        sum_vec = _mm256_add_epi32(sum_vec, prod);
+    }
+    int part2 = horizontal_sum(sum_vec);
+
+    int result = part1 + part2;
+
+    nnue->eval = ((((result) / QA) * SCALE) / QB) * EXTRA_SCALE;
+    nnue->eval = (color == WHITE) ? nnue->eval : -nnue->eval;
+}
+
+void setNNUEInput(S16* inputs, S32* accumulators, S32 (*weights)[INNER_LAYER_COUNT], int index) {
+    if (inputs[index] == 1)
+        return;
+
+    inputs[index] = 1;
+    for (int i = 0; i < INNER_LAYER_COUNT; i += 8) {
+        __m256i acc = _mm256_loadu_si256((__m256i*)&accumulators[i]);
+        __m256i w1  = _mm256_loadu_si256((__m256i*)&weights[index][i]);
+        acc = _mm256_add_epi32(acc, w1);
+        _mm256_storeu_si256((__m256i*)&accumulators[i], acc);
+    }
+}
+
+void resetNNUEInput(S16* inputs, S32* accumulators, S32 (*weights)[INNER_LAYER_COUNT], int index) {
+    if (inputs[index] == 0)
+        return;
+
+    inputs[index] = 0;
+    for (int i = 0; i < INNER_LAYER_COUNT; i += 8) {
+        __m256i acc = _mm256_loadu_si256((__m256i*)&accumulators[i]);
+        __m256i w1  = _mm256_loadu_si256((__m256i*)&weights[index][i]);
+        acc = _mm256_sub_epi32(acc, w1);
+        _mm256_storeu_si256((__m256i*)&accumulators[i], acc);
+    }
+}
+
+#endif
 
 void setDirectNNUEInput(NNUE* nnue, int index) {
   setNNUEInput(nnue->inputs, nnue->accumulators, nnue->weights_1_quantized, index);
